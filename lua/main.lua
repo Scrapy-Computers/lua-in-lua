bd4b4e92c65fb46ecd9a7e980cb15284fc7089f9
Nothing Mod <nikodemonikodemo@gmail.com>
2026-03-22 23:58:06 +0100

    Rework

--- lua/main.lua
+++ lua/main.lua
@@ -1,56 +1,62 @@
 dofile("scanner.lua")
 dofile("parser.lua")
 dofile("interpreter.lua")
-local m = {
-    Scanner = lua_scanner,
-    Parser = lua_parser,
-    Interpreter = lua_interpreter
-}
 
+function createContainer(settings)
+    settings = settings or {}
 
-
----Parses string input and returns a tree
----@param input string
----@return table "Tree representation"
-function m:getTree(input)
-    local tokens = self.Scanner:scan(input)
-    return self.Parser:parse(tokens)
-end
-
----
----Runs a lua string, `environment` should be a table with variables and functions that will be used as the global environment
----
----@param input string
----@param environment table
----@return ...
-function m:run(input, environment, ...)
-    local tokens = self.Scanner:scan(input)
-    local tree = self.Parser:parse(tokens)
-    local env = self.Interpreter:encloseEnvironment(environment)
-    self.Interpreter:setEnvMeta(env, "varargs", {...})
-    env.arg = {...}
-    return self.Interpreter:evaluate(tree, env)
-end
-
----
----Runs a lua file, `environment` should be a table with variables and functions that will be used as the global environment
----
----@param file_path string
----@param environment table
----@return ...
-function m:dofile(file_path, environment, ...)
-    dofile(file_path)
-    local source = lua_dofile
-    lua_dofile = nil
-    return self:run(source, environment, ...)
-end
-
-function m:tick()
-    for i, loop in ipairs(self.Interpreter.loops) do
-            if  loop() then
-                table.remove(self.Interpreter.loops, i)
-            end
+    local m = {
+        Scanner = lua_scanner,
+        Parser = lua_parser,
+        Interpreter = lua_interpreter(settings)
+    }
+    
+    ---Parses string input and returns a tree
+    ---@param input string
+    ---@return table "Tree representation"
+    function m:getTree(input)
+        local tokens = self.Scanner:scan(input)
+        return self.Parser:parse(tokens)
     end
+    
+    ---
+    ---Runs a lua string, `environment` should be a table with variables and functions that will be used as the global environment
+    ---
+    ---@param input string
+    ---@param environment table
+    ---@return ...
+    function m:run(input, environment, ...)
+        local tokens = self.Scanner:scan(input)
+        local tree = self.Parser:parse(tokens)
+        local env = self.Interpreter:encloseEnvironment(environment)
+        self.Interpreter:setEnvMeta(env, "varargs", {...})
+        env.arg = {...}
+        return self.Interpreter:evaluate(tree, env)
+    end
+    
+    ---
+    ---Runs a lua file, `environment` should be a table with variables and functions that will be used as the global environment
+    ---
+    ---@param file_path string
+    ---@param environment table
+    ---@return ...
+    function m:dofile(file_path, environment, ...)
+        dofile(file_path)
+        local source = lua_dofile
+        lua_dofile = nil
+        return self:run(source, environment, ...)
+    end
+    
+    function m:tick()
+        for i, loop in ipairs(self.Interpreter.loops) do
+                if loop() then
+                    table.remove(self.Interpreter.loops, i)
+                end
+        end
+    end
+    
+    return m
 end
 
-lua_in_lua = m
+lua_in_lua = createContainer()
+lua_in_lua.createContainer = createContainer
